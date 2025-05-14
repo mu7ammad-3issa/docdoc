5bcc5dbd53c76c314d9a9be7f16844d53e6a8660
Muhammad Issa <mu7ammad.3issa@gmail.com>
2025-05-14 11:18:29 +0300

    - Implement HomeScreen layout - add DoctorsBlueContainer - add DoctorsSpecialitySeeAll widgets

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,10 +1,37 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/helpers/spacing.dart';
+import 'package:flutter_complete_project/features/home/ui/widgets/doctors_blue_container.dart';
+import 'package:flutter_complete_project/features/home/ui/widgets/doctors_speciality_see_all.dart';
+import 'package:flutter_complete_project/features/home/ui/widgets/home_top_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          margin: const EdgeInsets.fromLTRB(
+            20.0,
+            16.0,
+            20.0,
+            28.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const HomeTopBar(),
+              const DoctorsBlueContainer(),
+              verticalSpace(24),
+              const DoctorsSpecialitySeeAll(),
+              verticalSpace(18),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
